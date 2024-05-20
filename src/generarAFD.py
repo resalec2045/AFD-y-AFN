2d8baf316fb35471d025123a7c854efb3417cb8e
David Velasco <david.santiago.velasco.12@gmail.com>
2024-05-20 16:43:03 -0500

    arreglo bug del ciclo

--- src/generarAFD.py
+++ src/generarAFD.py
@@ -1,20 +1,12 @@
+from PIL import Image
 from automathon import DFA
-
 
 class GenerarAFD:
 
     @staticmethod
     def crear_automata(palabra):
-
-        # print(f"Creando autómata para la palabra '{palabra}'")
-
-        # Definir los estados
         estados = {'q' + str(i) for i in range(len(palabra) + 1)}
-
-        # Definir el alfabeto sigma como el conjunto de caracteres de la palabra
         sigma = set(palabra)
-
-        # Definir la función de transición delta
         delta = {}
         for i, letra in enumerate(palabra):
             estado = 'q' + str(i)
@@ -22,42 +14,37 @@ class GenerarAFD:
             if estado not in delta:
                 delta[estado] = {}
             delta[estado][letra] = siguiente_estado
-
-        # Asegurarse de que cada estado tenga una transición para cada símbolo en sigma
-        for estado in estados:
-            if estado not in delta:
-                delta[estado] = {}
-            for simbolo in sigma:
-                if simbolo not in delta[estado]:
-                    delta[estado][simbolo] = estado  # Transición a sí mismo si no está definido
-
-        # Definir el estado inicial
         estado_inicial = 'q0'
-
-        # Definir el estado final
         estado_final = {'q' + str(len(palabra))}
-
         return estados, sigma, delta, estado_inicial, estado_final
 
     @staticmethod
     def mostrar_automata(palabra):
-        # Crear los componentes del autómata a partir de la palabra
         estados, sigma, delta, estado_inicial, estado_final = GenerarAFD.crear_automata(palabra)
-
-        # Mostrar los detalles del autómata
-        # print(f"Estados: {estados}")
-        # print(f"Alfabeto: {sigma}")
-        # print(f"Función de transición: {delta}")
-        # print(f"Estado inicial: {estado_inicial}")
-        # print(f"Estados finales: {estado_final}")
-
-        # Crear el autómata DFA
         automata = DFA(estados, sigma, delta, estado_inicial, estado_final)
-
-        # Graficar el autómata (si la librería automathon permite esta funcionalidad)
         automata.view(
             file_name="automatas/" + palabra,
             node_attr={'fontsize': '20'},
             edge_attr={'fontsize': '20pt'}
         )
+
+    @staticmethod
+    def combinar_imagenes(palabras):
+        images = []
+        for palabra in palabras:
+            image = Image.open("automatas/" + palabra + ".gv.png")
+            images.append(image)
+
+        # Asumiendo que todas las imágenes tienen el mismo tamaño
+        width, height = images[0].size
+
+        # Crear una imagen nueva con el tamaño adecuado para todas las imágenes
+        combined_image = Image.new('RGB', (width * len(palabras), height))
+
+        # Pegar cada imagen en la imagen combinada
+        for i, img in enumerate(images):
+            combined_image.paste(img, (i * width, 0))
+
+        # Guardar la imagen combinada
+        combined_image.save("automatas/combined_automatas.png")
 
